0eca12ee01f9833ea890a9588ee1f8129c85d8ce
GiftiaHuf2020 <65272429+GiftiaHuf2020@users.noreply.github.com>
2021-04-05 11:57:52 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Confession1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Location = new System.Drawing.Point(87, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "可以啊";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(146, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "小姐姐美若我心上人";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(175, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "做我女朋友好不好";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(113, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(353, 9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(148, 140);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(513, 161);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "来自一位喜欢你的小哥哥";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Confession1
+{
+    public partial class Form1 : Form
+    {
+        Boolean EnableClose = false;
+
+        Button newButton;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            newButton = new NoFocusCueButton();
+            this.Controls.Add(newButton);
+            newButton.Name = "buttonX";
+            newButton.Text = "不行";
+            newButton.BringToFront();
+            newButton.Location = new Point(249, 117);
+            newButton.Size = new Size(130, 30);
+            newButton.BackColor = Color.Transparent;
+            newButton.ForeColor = Color.DodgerBlue;
+            newButton.FlatStyle = FlatStyle.Flat;
+            newButton.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            newButton.FlatAppearance.MouseDownBackColor = Color.Transparent;
+
+            foreach (Button btn in Controls.OfType<Button>())
+            {
+                btn.MouseEnter += new System.EventHandler(btn_MouseEnter);
+                btn.MouseDown += new System.Windows.Forms.MouseEventHandler(btn_MouseDown);
+            }
+
+
+        }
+
+        private void btn_MouseDown(object sender, System.EventArgs e)
+        {
+            var senderButton = (Button)sender;
+
+            //Console.WriteLine(senderButton);
+            if (senderButton.Name == "button1")
+            {
+                //Do Nothing
+                //Console.WriteLine("Button 1 here");
+            }
+            else
+            {
+                //Console.WriteLine(newButton.Location);
+                Random r = new Random();
+                int rxInt = r.Next(0, 400); //for Random X
+                int ryInt = r.Next(0, 150); //for Random Y
+
+                newButton.Location = new Point(rxInt, ryInt);
+            }
+        
+        }
+
+        private void btn_MouseEnter(object sender, System.EventArgs e)
+        {
+            var senderButton = (Button)sender;
+
+            if (senderButton.Name == "button1") {
+                //Do Nothing
+                //Console.WriteLine("Button 1 here");
+            }
+            else {
+                //Console.WriteLine(newButton.Location);
+                Random r = new Random();
+                int rxInt = r.Next(0, 400); //for Random X
+                int ryInt = r.Next(0, 150); //for Random Y
+                
+                newButton.Location = new Point(rxInt, ryInt);
+                
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove(newButton);
+            string message = "同意不就可以啦，么么哒~";
+            string title = "亲爱的";
+            MessageBox.Show(message, title);
+            
+            EnableClose = true;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!EnableClose)
+            {
+                string message = "不答应我你关不掉的噢~";
+                string title = "答应我嘛";
+                MessageBox.Show(message, title);
+                e.Cancel = true;
+            }
+
+        }
+    }
+
+    public class NoFocusCueButton : Button
+    {
+        public NoFocusCueButton() : base()
+        {
+
+            this.SetStyle(ControlStyles.Selectable, false);
+        }
+
+        protected override bool ShowFocusCues
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
